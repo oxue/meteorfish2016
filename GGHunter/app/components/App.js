34179ae49de39fd0a5a640894035f8a125317a7c
Eric Kim <bosung90@gmail.com>
2016-04-23 14:44:37 -0700

    added tabor

--- GGHunter/app/components/App.js
+++ GGHunter/app/components/App.js
@@ -9,15 +9,30 @@ import React, {
   View,
   Geolocation,
 } from 'react-native'
+import {Scene, Router, TabBar, Modal, Schema, Actions, Reducer} from 'react-native-router-flux'
 import CameraPage from './CameraPage'
 import MapPage from './MapPage'
+
+class TabIcon extends React.Component {
+  render(){
+    return (
+      <Text style={{color: this.props.selected ? 'red' :'black'}}>{this.props.title}</Text>
+    )
+  }
+}
 
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <MapPage />
-      </View>
+      <Router sceneStyle={{backgroundColor:'#F7F7F7'}}>
+        <Scene key="root" hideNavBar={true}>
+          <Scene key="tabbar" tabs={true} >
+            <Scene key="tab3" component={MapPage} title="Tab #3" icon={TabIcon}/>
+            <Scene key="tab4" component={MapPage} title="Tab #4" icon={TabIcon}/>
+            <Scene key="tab5" component={CameraPage} title="Tab #5" icon={TabIcon} />
+          </Scene>
+        </Scene>
+      </Router>
     )
   }
 }
