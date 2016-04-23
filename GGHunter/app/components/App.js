b40605c6a05603a6f41dca1a3349f01780e29a95
Eric Kim <bosung90@gmail.com>
2016-04-23 03:03:36 -0700

    added camera

--- GGHunter/app/components/App.js
+++ GGHunter/app/components/App.js
@@ -1,18 +1,58 @@
 import React, {
   AppRegistry,
   Component,
-  View,
+  Dimensions,
+  StyleSheet,
   Text,
+  TouchableHighlight,
+  View,
 } from 'react-native'
+import Camera from 'react-native-camera'
 
 export default class App extends Component {
-  render(){
+  render() {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>Hello World</Text>
+      <View style={styles.container}>
+        <Camera
+          ref={(cam) => {
+            this.camera = cam
+          }}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+        </Camera>
+        <View style={styles.captureView}>
+          <Text style={{color: 'black'}} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </View>
       </View>
     )
   }
+  takePicture() {
+    this.camera.capture()
+      .then((data) => console.log(data))
+      .catch(err => console.error(err))
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    height: Dimensions.get('window').height,
+    width: Dimensions.get('window').width
+  },
+  captureView: {
+    flex:1,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: 0,
+    right: 0,
+    bottom: 60,
+  },
+})
 
 AppRegistry.registerComponent('GGHunter', () => App)
