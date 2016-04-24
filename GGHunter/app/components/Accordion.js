cb0ad32fe52b78f70a9b1ddc075084816befdac0
Edward Mason <esmason66@gmail.com>
2016-04-24 02:32:47 -0700

    accordion entry for date

--- GGHunter/app/components/Accordion.js
+++ GGHunter/app/components/Accordion.js
@@ -33,9 +33,9 @@ export default class AccordionPage extends Component {
   constructor(props, context){
     super(props, context)
     this.state = {
-      date: '',
-      month: '',
-      year: '',
+      date: 'Date',
+      month: 'Month',
+      year: 'Year',
       region: {
         latitude: 49.30080957108101,
         longitude: -123.1337930524565,
@@ -56,21 +56,29 @@ export default class AccordionPage extends Component {
     return (
       <View style={styles.header}>
         <Text style={styles.firstText}>{section.firstln}</Text>
-        <Text style={styles.secondText}>{this.state}</Text>
+        <Text style={styles.secondText}>{this.state.date + ' ' + this.state.month + ' ' + this.state.year}</Text>
       </View>
     )
   }
   _renderContent(section) {
     return (
       <View style={styles.content}>
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+      <TextInput ref = 'Date'
+        style={{height: 50, width: 60, borderColor: 'gray', borderWidth: 1}}
         onChangeText={(text) => this.setState({date: text})}
-        value={this.state.date}
-
-
-      />
-      </View>
+        maxLength = {2}
+        placeholder='DD'/>
+        <TextInput ref = 'Month'
+          style={{height: 50, width: 60, borderColor: 'gray', borderWidth: 1}}
+          onChangeText={(text) => this.setState({month: text})}
+          maxLength = {2}
+          placeholder='MM'/>
+      <TextInput ref = 'Year'
+        style={{height: 50, width: 60, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({year: text})}
+        maxLength = {4}
+        placeholder='YYYY'/>
+    </View>
     )
   }
   componentWillMount(){
@@ -142,7 +150,7 @@ export default class AccordionPage extends Component {
           sections={SECTIONS}
           initiallyActiveSection = {0}
           duration = {350}
-          renderHeader={this._renderHeader}
+          renderHeader={this._renderHeader.bind(this)}
           renderContent={this._renderContent.bind(this)}
         />
       </ScrollView>
