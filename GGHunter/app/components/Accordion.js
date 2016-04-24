bedb87d735df77538fda5c7ee085cfa04404af69
Edward Mason <esmason66@gmail.com>
2016-04-24 01:27:06 -0700

    accordian changes

--- GGHunter/app/components/Accordion.js
+++ GGHunter/app/components/Accordion.js
@@ -1,7 +1,10 @@
 import React, {
   Component,
+  DatePickerAndroid,
+  Easing,
   View,
   Text,
+  TextInput,
   StyleSheet,
   LayoutAnimation,
 } from 'react-native'
@@ -9,37 +12,60 @@ const Accordion = require('react-native-collapsible/Accordion')
 
 const SECTIONS = [
   {
-    title: 'First',
+    firstln: 'Date ghost gear discovered',
+    secondln: Date(),
     content: 'Lorem ipsum...',
   },
   {
-    title: 'Second',
+    firstln: 'Colour',
+    secondln: 'Grey',
     content: 'Lorem ipsum...',
   }
+
 ]
 
 export default class AccordionPage extends Component {
+  constructor(props, context){
+    super(props, context)
+    this.state = {
+      date: '',
+      month: '',
+      year: '',
+    }}
+
+
   _renderHeader(section) {
     return (
       <View style={styles.header}>
-        <Text style={styles.headerText}>{section.title}</Text>
+        <Text style={styles.firstText}>{section.firstln}</Text>
+        <Text style={styles.secondText}>{this.state}</Text>
       </View>
     )
   }
+
   _renderContent(section) {
     return (
       <View style={styles.content}>
-        <Text>{section.content}</Text>
+      <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(text) => this.setState({date: text})}
+        value={this.state.date}
+
+
+      />
       </View>
     )
   }
+
   render() {
     return (
       <View style={styles.container}>
         <Accordion
           sections={SECTIONS}
+          initiallyActiveSection = {0}
+          duration = {350}
           renderHeader={this._renderHeader}
-          renderContent={this._renderContent}
+          renderContent={this._renderContent.bind(this)}
         />
       </View>
     )
@@ -49,10 +75,30 @@ export default class AccordionPage extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 64,
+    paddingTop: 100,
   },
   header: {
-    height: 40,
-    backgroundColor: 'red',
+    height: 60,
+    backgroundColor: '#6161c2',
+  },
+  content: {
+    height: 60,
+    backgroundColor: 'white',
+  },
+  firstText: {
+    fontSize : 14,
+    marginLeft: 40,
+    marginTop: 10,
+
+    color : '#fff',
+    textAlign : 'left'
+  },
+  secondText: {
+    fontSize : 14,
+    marginLeft: 40,
+
+    color : '#c5c5c5',
+    textAlign : 'left'
   }
+
 })
